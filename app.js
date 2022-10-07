1a190264c90d38c1523206e2a90e6b2725d3e2af
CDarioB <carlosdario1@gmail.com>
2022-10-07 20:28:45 -0300

    clase 12 a 16

--- app.js
+++ app.js
@@ -6,11 +6,23 @@ const app = express();
 const publicPath = path.resolve(__dirname,'./public');
 app.use(express.static(publicPath));
 
-
+//const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'))
 });
 
+app.get("/register", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/register.html"))
+});
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/login.html"))
+});
+
 
 app.listen (3030, () => console.log ("Server started: http://localhost:3030/") ); 
+
+// app.listen (PORT, () => console.log (`Server started: port ${PORT}`) ); 
+
+// html tag wrapper
